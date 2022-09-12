4096d7cde7be93c15b707ae7a96ebd4a3e215e3e
DNML31 <danielchun31@gmail.com>
2022-09-12 16:00:32 -0700

    Change if... else if for operator choices
    Going to use switch expression for the choice of operator instead of an
    if...else if block.

--- javascript.js
+++ javascript.js
@@ -3,92 +3,85 @@ const subtract = (a,b) => +a - +b;
 const multiply = (a,b) => +a * +b;
 const divide = (a,b) => +a / +b;
 
-const operate = function () { 
-  let operateObj = {};
-  operateObj.a = prompt("first number?");
-  operateObj.op = prompt("add? subtract? multiply? divide?");
-  //how can i link the prompt answer to the object key:value?
-  operateObj.b = prompt("second number?");
+// const operate = function () { 
+//   let operateObj = {};
+//   operateObj.a = prompt("first number?");
+//   operateObj.op = prompt("add? subtract? multiply? divide?");
+//   //how can i link the prompt answer to the object key:value?
+//   operateObj.b = prompt("second number?");
   
-  if (operateObj.op === "add") {
-    return add(operateObj.a, operateObj.b);
-  } else if (operateObj.op === "subtract") {
-    return subtract(operateObj.a, operateObj.b);
-  } else if (operateObj.op === "multiply") {
-    return multiply(operateObj.a, operateObj.b);
-  } else if (operateObj.op === "divide") {
-    return divide(operateObj.a, operateObj.b);
-  }
-}
+//   if (operateObj.op === "add") {
+//     return add(operateObj.a, operateObj.b);
+//   } else if (operateObj.op === "subtract") {
+//     return subtract(operateObj.a, operateObj.b);
+//   } else if (operateObj.op === "multiply") {
+//     return multiply(operateObj.a, operateObj.b);
+//   } else if (operateObj.op === "divide") {
+//     return divide(operateObj.a, operateObj.b);
+//   }
+// }
 
 let display = document.getElementById('display');
-let numberA; //the first number needs to assigned to the 'a' in operator functions
+let displayOp = document.getElementById('operation');
+let numberA; 
 let numberB;
 
-//if numbers are clicked, keep reassigning value of numberA, until an operator is clicked,
-const buttons = document.querySelectorAll('button'); // selecting ALL buttons
+const buttons = document.querySelectorAll('button'); 
 buttons.forEach((button) => {
   button.addEventListener ('click', () => {
-    if (button.classList.contains('number')) {
-
+    if (button.classList.contains('number')) { 
       let char = button.textContent;  
       display.append(char);
 
     } else if (button.classList.contains('buttonoperator') && display.hasChildNodes() === false) { 
-
       return;
-
-    } else if (display.hasChildNodes() && button.classList.contains('buttonclear')) {
-
+      
+    } else if (button.classList.contains('buttonclear') && display.hasChildNodes() === true) {
       while (display.hasChildNodes()) {
         display.removeChild(display.firstChild);
       }
+      while (displayOp.hasChildNodes()) {
+        displayOp.removeChild(displayOp.firstChild);
+      }
     
     } else if (button.classList.contains('buttonoperator') && display.hasChildNodes() === true) {
-
       let numberA = display;
-      let op = button.textContent; // determines which operator function to use
-      let operation;
-
-      if (op === "+") {
-        let operation = add();
-      } else if (op === "-") {
-        let operation = subtract();
-      } else if (op === "*") {
-        let operation = multiply();
-      } else if (op === "/") {
-        let operation = divide();
+      // let op = button.textContent;
+      displayOp.append(op);
+      //need to limit the number of operators at a time to 1
+      if (displayOp.length === 1) {
+        alert("hey");
       }
 
-      let char = button.textContent;
-      display = display.append(char);
+      switch (op = button.textContent) {
+        case "+":
+          displayOp.append(op);
+          add();
+          break; 
+        case "-":
+          displayOp.append(op);
+          subtract();
+          break;
+        case "*":
+          displayOp.append(op);
+          multiply();
+          break;
+        case "/":
+          displayOp.append(op);
+          divide();
+          break;
 
+      }
 
-       return operation(numberA, numberB);
+      console.log(numberA);
+      // let char = button.textContent;
+      // display.append(char);
+      // let numberB = display;
 
     }
+
   });
 })
 
-
-// const operators = document.querySelectorAll(".buttonoperator");
-// operators.forEach((operator) => {
-//   operator.addEventListener ('click', () => {
-//     let op = operator.textContent;
-//       //link the 'operator' to the matching function const
-//     if (op === "+") {
-//       add();
-//     } else if (op === "-") {
-//       subtract();
-//     } else if (op === "*") {
-//       multiply();
-//     } else if (op === "/") {
-//       divide();
-//     }
-
-//   })
-// })
-
-
-//Create a new function operate that takes an operator and 2 numbers and then 
-//calls one of the above functions on the numbers.
+//line 62 - 'numberA' is logged as individual number strings. 99 is "9" and "9"
+//only one operator should be selected at max
