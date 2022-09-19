ae7f1553bd36c74d9a13869e98eb38426a164003
DNML31 <danielchun31@gmail.com>
2022-09-19 10:32:42 -0700

    Reworking event listeners
    Currently have issues with event listeners for numbers and operators.
    
    Operator event listener - using a named function for the listener
    instead of an anonymous arrow function as I have been. All operators
    keep showing up on display div at page startup without being clicked.
    
    Number event listener - numbers do not display correctly at page
    startup, while operators are on display. If the operators are cleared
    with the clear button and then numbers are clicked, they will display
    correctly but operators won't be clickable after that. Numbers can then
    be cleared and will display correctly when clicked.
    
    *** Decimal has not been worked in yet.

--- javascript.js
+++ javascript.js
@@ -23,113 +23,70 @@ const divide = (a,b) => +a / +b;
 
 const displayOp = document.getElementById('operation');
 const display = document.getElementById('display');
+let numDiv = document.createElement('div');
 let value;
 let numberA;
 let numberB;
 
-// const buttons = document.querySelectorAll('button'); 
-// buttons.forEach((button) => { 
-//   button.addEventListener ('click', () => {
-
-//     if (button.classList.contains('number')) { 
-//       let numberA = button.textContent;
-//       display.append(numberA);
-
-//     } else if (button.classList.contains('operator') && display.hasChildNodes()) { 
-//       return;
-      
-//     } else if (button.classList.contains('buttonclear') && display.hasChildNodes()) {
-//       while (display.hasChildNodes()) {
-//         display.removeChild(display.firstChild);
-//       }
-//       while (displayOp.hasChildNodes()) {
-//         displayOp.removeChild(displayOp.firstChild);
-//       }
-//     }
-    
-//     if (button.classList.contains('operator') && display.hasChildNodes()) {
-//       let numberA = display.textContent;
-//       let op = button.textContent;
-//       displayOp.append(op);
-//       //need to limit the number of operators at a time to 1
-//     } else if (button.classList.contains('operator') && displayOp.hasChildNodes()) {
-//       while (displayOp.hasChildNodes()) {
-//         displayOp.removeChild(displayOp.firstChild);
-//       }
-//       displayOp.append(op);
-//     }
-
-//     if (button.classList.contains('number') && displayOp.hasChildNodes() && display.hasChildNodes()) {
-//       let numberB = display.textContent;
-//       display.append(numberB);
-//     }
-//   });
-// })
-
 
 const numbers = document.querySelectorAll('.number');
 numbers.forEach((number) => {
-  number.addEventListener ('click', () => {
-    let numDiv = document.createElement('div');
+
+  number.addEventListener('click', () => {
+
+    if (display.hasChildNodes() && displayOp.hasChildNodes()) {
+      while (display.hasChildNodes()) {
+        display.removeChild(display.firstChild);
+      };
+    
+      const numbers = document.querySelectorAll('.number');
+
+      numbers.forEach((number) => {
+        number.addEventListener('click', () => {
+          numDiv = number.textContent;
+          display.append(numDiv);
+          numberB = numDiv;
+          console.log(numberB);
+        })
+      })
+    }
     numDiv = number.textContent;
     display.append(numDiv);
-
-    let numberA = numDiv;
+    numberA = numDiv;
     console.log(numberA);
 
-    if (displayOp.hasChildNodes()) {
-      display.removeChild(numDiv);
-      let numDiv = document.createElement('div');
-      numDiv = number.textContent;
-      display.append(numDiv);
-
-      let numberB = numDiv;
-      console.log(numberB);
-
-    } else if (display.hasChildNodes() && displayOp.hasChildNodes()) {
-      let numDiv = document.createElement('div');
-      value = number.textContent;
-      display.append(value);
-    }
   })
-
-
 });
-//add numbers to a value when clicked
-//if an operator button has been clicked, add them to another value
 
 
 const operators = document.querySelectorAll('.operator');
 operators.forEach((operator) => {
-  operator.addEventListener ('click', () => {
-    let O = document.createElement('div');
-    O = operator.textContent;
-    displayOp.append(O);
-  })
-  if (display.hasChildNodes()) {
-    value = numberA;
-  }
-})
+  operator.addEventListener('click', opBtn(operator));
+});
 
-if (displayOp.hasChildNodes()) {
-  operator.removeEventListener('click', () => {
-    let O = document.createElement('div');
-    O = operator.textContent;
-    displayOp.append(O);
-  })
+
+function opBtn(x) {
+  let opDiv = document.createElement('div');
+  opDiv = x.textContent;
+  displayOp.append(opDiv);
 }
+//all operators just show up on display without click
+
+
+
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', () => {
+  while (displayOp.hasChildNodes()) {
+    displayOp.removeChild(displayOp.firstChild);
+  }
+  while (display.hasChildNodes()) {
+    display.removeChild(display.firstChild);
+  }
+  
+  numDiv = undefined;
+  numberA = undefined;
+  numberB = undefined;
+});
+
 
 const solution = document.getElementById('equals');
-//run the function of the operator that was chosen with the two values
-
-
-// const clearButton = document.getElementById('clear');
-// clearButton.addEventListener('click', () => {
-//   while (display.hasChildNodes()) {
-//     display.removeChild(display.firstChild);
-//   }
-
-//   while (displayOp.hasChildNodes()) {
-//     displayOp.removeChild(displayOp.firstChild);
-//   }
-// });
